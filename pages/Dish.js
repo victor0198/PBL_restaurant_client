ed99cce3d67b4fe7cd4db1373425cd386aa78434
victor0198utm <caragiu.victor@isa.utm.md>
2020-11-30 02:23:56 +0200

    added pages

---
+++ pages/Dish.js
@@ -0,0 +1,13 @@
+import {} from 'antd'
+import {} from '@ant-design/icons'
+
+const Dish = () => {
+  return (
+    <div className="dish">
+        <p>image</p>
+        <h1>Dish name</h1>
+    </div>
+  )
+}
+
+export default Dish;
---
+++ pages/QRScanner.js
@@ -0,0 +1,13 @@
+import {} from 'antd'
+import {} from '@ant-design/icons'
+
+const QRScanner = () => {
+  return (
+    <div className="scanner">
+        <h1>Scan the QR code</h1>
+        <div className="cam_view">camera view</div>
+    </div>
+  )
+}
+
+export default QRScanner;
