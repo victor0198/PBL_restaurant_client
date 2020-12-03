0ea49aab9d74b8354194f56a7d45ac811ec8b1d3
victor0198utm <caragiu.victor@isa.utm.md>
2020-12-03 22:26:04 +0200

    created the layout and styles for 5 pages

---
+++ pages/Category.js
@@ -0,0 +1,108 @@
+import {Button} from 'antd'
+import {LeftOutlined} from '@ant-design/icons'
+
+const Category = () => {
+  return (
+    <>
+        <div className="category">
+            <div className="background">
+                <div className="get_overlay">
+                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.delonghi.com%2FGlobal%2Frecipes%2Fmultifry%2F3.jpg&f=1&nofb=1"></img>
+                </div>
+                
+                <Button>
+                    <LeftOutlined />
+                </Button>
+                <div className="category_name">
+                    <p>Pizzas</p>
+                </div>
+                
+            </div>
+            <div className="list">
+
+                <div className="dish">
+                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.readersdigest.ca%2Fwp-content%2Fuploads%2Fsites%2F14%2F2010%2F12%2Fpepperoni-pizza.jpg&f=1&nofb=1"></img>
+                    <div className="dish_content">
+                        
+                        <div className="text">
+                            <p className="name">Peperroni</p>
+                            <div className="price">
+                                <p>Price</p>
+                                <p>99 MDL</p>
+                            </div>
+                            <Button>Add</Button>
+                        </div>
+                    </div>
+                </div>
+                <div className="dish">
+                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.readersdigest.ca%2Fwp-content%2Fuploads%2Fsites%2F14%2F2010%2F12%2Fpepperoni-pizza.jpg&f=1&nofb=1"></img>
+                    <div className="dish_content">
+                        
+                        <div className="text">
+                            <p className="name">Peperroni</p>
+                            <div className="price">
+                                <p>Price</p>
+                                <p>99 MDL</p>
+                            </div>
+                            <Button>Add</Button>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className="dish">
+                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.readersdigest.ca%2Fwp-content%2Fuploads%2Fsites%2F14%2F2010%2F12%2Fpepperoni-pizza.jpg&f=1&nofb=1"></img>
+                    <div className="dish_content">
+                        
+                        <div className="text">
+                            <p className="name">Peperroni</p>
+                            <div className="price">
+                                <p>Price</p>
+                                <p>99 MDL</p>
+                            </div>
+                            <Button>Add</Button>
+                        </div>
+                    </div>
+                </div>
+                <div className="dish">
+                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.readersdigest.ca%2Fwp-content%2Fuploads%2Fsites%2F14%2F2010%2F12%2Fpepperoni-pizza.jpg&f=1&nofb=1"></img>
+                    <div className="dish_content">
+                        
+                        <div className="text">
+                            <p className="name">Peperroni</p>
+                            <div className="price">
+                                <p>Price</p>
+                                <p>99 MDL</p>
+                            </div>
+                            <Button>Add</Button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="dish">
+                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.readersdigest.ca%2Fwp-content%2Fuploads%2Fsites%2F14%2F2010%2F12%2Fpepperoni-pizza.jpg&f=1&nofb=1"></img>
+                    <div className="dish_content">
+                        
+                        <div className="text">
+                            <p className="name">Peperroni</p>
+                            <div className="price">
+                                <p>Price</p>
+                                <p>99 MDL</p>
+                            </div>
+                            <Button>Add</Button>
+                        </div>
+                    </div>
+                </div>
+                
+
+
+            </div>
+            
+
+            
+
+        </div>
+    </>
+  )
+}
+
+export default Category;
--- pages/Room.js
+++ pages/Room.js
@@ -1,17 +1,90 @@
-import {} from 'antd'
-import {} from '@ant-design/icons'
+import {Button, Avatar} from 'antd'
+import {LeftOutlined, UpOutlined, DownOutlined} from '@ant-design/icons'
+import { useState } from 'react';
 
 const Room = () => {
+  const [showFriendsOrders, setShowFO] = useState(true);
+
   return (
     <div className="room">
-        <h1>Orders in this room</h1>
+        <div className="header">
+          <Button>
+            <LeftOutlined />
+          </Button>
+          <p>My order</p>
+        </div>
         <div className="content">
-            <div className="order">
-                <div className="user">username</div>
+            <div className="my_order">
                 <div className="dishes">
-                  <div className="dish">dish 1</div>
+                  <div className="dish">
+                    <p className="dish_name">item 1</p>
+                    <p className="dish_price">__MDL</p>
+                  </div>
+                  <div className="dish">
+                    <p className="dish_name">item 1</p>
+                    <p className="dish_price">__MDL</p>
+                  </div>
+                </div>
+                <p className="yt">TOTAL: __ MDL</p>
+                <div className="btns">
+                  <Button className="b1">MODIFY</Button>
+                  <Button className="b2">ACCEPT</Button>
                 </div>
             </div>
+
+              {showFriendsOrders?
+                <div className="fo">
+                  <p>Show friends' orders</p>
+                  <Button className="sm_btn" onClick={(e) => {setShowFO(false)}}>
+                    <DownOutlined className="separator"/>
+                  </Button>
+                </div>
+                :
+                <div className="fo">
+                  <Button onClick={(e) => {setShowFO(true)}}>
+                    <UpOutlined className="separator"/>
+                  </Button>
+
+
+                  <div className="order">
+                    <div className="user">
+                      <Avatar  src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fginva.com%2Fwp-content%2Fuploads%2F2012%2F03%2Ffemale-portrait-photography-57.jpg&f=1&nofb=1" />
+                      <p className="nickname">Nicoletta</p>
+                    </div>
+                      <div className="dishes">
+                        <div className="dish">
+                          <p className="dish_name">item 1</p>
+                          <p className="dish_price">__MDL</p>
+                        </div>
+                        <div className="dish">
+                          <p className="dish_name">item 1</p>
+                          <p className="dish_price">__MDL</p>
+                        </div>
+                      </div>
+                  </div>
+                  <div className="order">
+                    <div className="user">
+                      <Avatar  src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fginva.com%2Fwp-content%2Fuploads%2F2012%2F03%2Ffemale-portrait-photography-57.jpg&f=1&nofb=1" />
+                      <p className="nickname">Nicoletta</p>
+                    </div>
+                      <div className="dishes">
+                        <div className="dish">
+                          <p className="dish_name">item 1</p>
+                          <p className="dish_price">__MDL</p>
+                        </div>
+                        <div className="dish">
+                          <p className="dish_name">item 1</p>
+                          <p className="dish_price">__MDL</p>
+                        </div>
+                      </div>
+                  </div>
+
+
+
+                </div>
+              }         
+
+            
         </div>
     </div>
   )
